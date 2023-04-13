65ba11061c83642e2a71dd9164e2fb33d8d1e4bf
ngharrison <7918794+ngharrison@users.noreply.github.com>
2023-04-13 14:28:18 +1000

    Make slfm docs notation simpler and more consistent
    Change to match notation used in other multi-output kernels

--- src/mokernels/slfm.jl
+++ src/mokernels/slfm.jl
@@ -5,10 +5,10 @@ Kernel associated with the semiparametric latent factor model.
 
 # Definition
 
-For inputs ``x, x'`` and output dimensions ``p_x, p_{x'}'``, the kernel is defined as[^STJ]
+For inputs ``x, x'`` and output dimensions ``p, p'``, the kernel is defined as[^STJ]
 ```math
-k\big((x, p_x), (x, p_{x'})\big) = \sum^{Q}_{q=1} A_{p_xq}g_q(x, x')A_{p_{x'}q}
-                                   + e\big((x, p_x), (x', p_{x'})\big),
+k\big((x, p), (x, p')\big) = \sum^{Q}_{q=1} A_{p q}g_q(x, x')A_{p' q}
+                                   + e\big((x, p), (x', p')\big),
 ```
 where ``g_1, \ldots, g_Q`` are ``Q`` kernels, one for each latent process, ``e`` is a
 multi-output kernel for ``m`` outputs, and ``A`` is a matrix of weights for the kernels of
